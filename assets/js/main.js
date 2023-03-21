7dd8491f8414ec5efeb9cd1395117b85b78e7db8
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-21 17:41:34 +0100

    add last commit ( alle 18 devo andare via)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 
 
 //Visualizzare in pagina 5 numeri casuali..
-let randomNumbers = []
+/* let randomNumbers = []
+ */
 
 
 
-/* clearInterval(mymyTimeout); */
-//Da lì parte un timer di 30 secondi
+//Da lì parte un timer di 30 second
 
 function myfunction() {
     
@@ -14,16 +14,34 @@ function myfunction() {
     
 
         let randNumber = Math.floor(Math.random() * 100) 
-        randomNumbers.push(randNumber);
+        /* randomNumbers.push(randNumber); */
         console.log(randNumber);
     
-        document.body.innerHTML = randNumber;
-       
+        document.getElementById('number').innerHTML = randNumber;
        
     }
+
+    
+
 }
 
-/* prompt(alert('inserisci il numero che hai visto')) */
+myfunction();
+
+function hideRandNumb() {
+    
+    
+    
+
+        document.body.innerHTML = ``;
+       
+    
+    
+
+}
+
+hideRandNumb();
+
+prompt(alert('inserisci il numero che hai visto'))
 //Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 
 
