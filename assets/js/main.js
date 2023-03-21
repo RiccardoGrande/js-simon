fdf43b063f278880f5728d44b76d799eb9227e66
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-21 14:30:58 +0100

    add start commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+
+//Visualizzare in pagina 5 numeri casuali..
+for (let index = 0; index < 5; index++) {
+
+    const randNumber = Math.floor(Math.random() * 5) 
+
+    console.log(randNumber);
+
+    
+}
+
+//Da lì parte un timer di 30 secondi
+
+
+
+//Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+
+
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
